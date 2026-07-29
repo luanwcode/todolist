eb30c16b428294f4f0a38f9b672e238bde14f050
Luan <luanwolski2008@gmail.com>
2026-07-29 17:25:49 -0300

    added the tasks template

--- tasks/views.py
+++ tasks/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def tasks_home(request):
-    return HttpResponse("Here are your tasks")
+    return render(request, 'tasks/home.html')
 
 def tasks_add(request):
     return HttpResponse("Here you can add your tasks")
