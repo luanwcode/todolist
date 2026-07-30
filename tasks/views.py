dabdff426b6ee1f42bdb944bb3a02fa924d8a3bb
Luan <luanwolski2008@gmail.com>
2026-07-30 12:15:07 -0300

    added a table to visualize created tasks

--- tasks/views.py
+++ tasks/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, redirect
 from django.http import HttpRequest
 from . forms import TaskForm
+from . models import TaskModel
 
 def tasks_home(request):
     context = {
-        "name": "Luan"
+        "name": "Luan",
+        "tasks": TaskModel.objects.all()
     }
 
     return render(request, 'tasks/home.html', context)
