30af2357ae6622f701b18a7d461fc23497887690
Luan <luanwolski2008@gmail.com>
2026-08-04 17:49:14 -0300

    improved form visuals using widgets

--- tasks/forms.py
+++ tasks/forms.py
@@ -5,3 +5,12 @@ class TaskForm(forms.ModelForm):
     class Meta:
         model = TaskModel
         fields = ['name', 'description', 'completed', 'status']
+
+        widgets = {
+            "name": forms.TextInput(attrs={
+                "class": "bg-neutral-800 text-white border border-neutral-700 rounded-md px-3 py-2 w-full"
+            }),
+            "description": forms.Textarea(attrs={
+                "class": "bg-neutral-800 text-white border border-neutral-700 rounded-md px-3 py-2 w-full"
+            }),
+        }
