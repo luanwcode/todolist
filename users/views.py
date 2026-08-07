dd90fb68cb573e398c2125c371b94e3dd34ff57e
Luan <luanwolski2008@gmail.com>
2026-08-07 16:37:23 -0300

    working register feature

--- users/views.py
+++ users/views.py
@@ -13,4 +13,12 @@ def users_register(request):
         email = request.POST.get('email')
         password = request.POST.get('password')
 
-        return HttpResponse(username)
+        user = User.objects.filter(username = username).first()
+
+        if user:
+            return HttpResponse('Username already registered')
+
+        user = User.objects.create_user(username=username, email=email, password=password)
+        user.save()
+
+        return HttpResponse('User sucessfully registered')
